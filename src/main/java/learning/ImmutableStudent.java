8332fa312569d31d1bc00106bdd9ac54709c3b42
Someshbose <iamsomeshbose@gmail.com>
2021-07-26 12:40:54 +0530

    checkstyle error fixed

--- src/main/java/learning/ImmutableStudent.java
+++ src/main/java/learning/ImmutableStudent.java
@@ -3,58 +3,85 @@ package learning;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Custom Immutable class example.
+ */
 public final class ImmutableStudent {
 
-    private final String name;
+  private final String name;
 
-    private final int regNo;
+  private final int regNo;
 
-    private final Map<String,String> metadata;
+  private final Map<String, String> metadata;
 
-    public ImmutableStudent(String name,int regNo,Map<String,String> metadata){
-        this.name=name;
-        this.regNo=regNo;
+  /**
+   * constructor
+   *
+   * @param name     String
+   * @param regNo    int
+   * @param metadata Map
+   */
+  public ImmutableStudent(String name, int regNo, Map<String, String> metadata) {
+    this.name = name;
+    this.regNo = regNo;
 
-        Map<String,String> tempMap = new HashMap<>();
+    Map<String, String> tempMap = new HashMap<>();
 
-        for (Map.Entry<String,String> entry:metadata.entrySet()){
-            tempMap.put(entry.getKey(),entry.getValue());
-        }
-        this.metadata=tempMap;
+    for (Map.Entry<String, String> entry : metadata.entrySet()) {
+      tempMap.put(entry.getKey(), entry.getValue());
     }
+    this.metadata = tempMap;
+  }
 
-    public String getName() {
-        return name;
+  /**
+   * getName method
+   *
+   * @return String
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @return int
+   */
+  public int getRegNo() {
+    return regNo;
+  }
+
+  /**
+   * getMetadata
+   *
+   * @return Map
+   */
+  public Map<String, String> getMetadata() {
+    Map<String, String> tempMap = new HashMap<>();
+
+    for (Map.Entry<String, String> entry : metadata.entrySet()) {
+      tempMap.put(entry.getKey(), entry.getValue());
     }
+    return tempMap;
+  }
 
-    public int getRegNo() {
-        return regNo;
-    }
+  /**
+   * main method
+   *
+   * @param args string array
+   */
+  public static void main(String[] args) {
+    Map<String, String> map = new HashMap<>();
 
-    public Map<String, String> getMetadata() {
-        Map<String,String> tempMap = new HashMap<>();
+    map.put("1", "first");
+    map.put("2", "second");
 
-        for (Map.Entry<String,String> entry:metadata.entrySet()){
-            tempMap.put(entry.getKey(),entry.getValue());
-        }
-        return tempMap;
-    }
+    ImmutableStudent student = new ImmutableStudent("ABC", 101, map);
 
-    public static void main(String[] args) {
-        Map<String,String> map = new HashMap<>();
+    System.out.println(student.getName());
+    System.out.println(student.getRegNo());
+    System.out.println(student.getMetadata());
 
-        map.put("1","first");
-        map.put("2","second");
+    map.put("3", "Third");
+    System.out.println(student.getMetadata());
 
-        ImmutableStudent student = new ImmutableStudent("ABC",101,map);
-
-        System.out.println(student.getName());
-        System.out.println(student.getRegNo());
-        System.out.println(student.getMetadata());
-
-        map.put("3","Third");
-        System.out.println(student.getMetadata());
-
-
-    }
+  }
 }
--- src/main/java/main/LRUCache.java
+++ src/main/java/main/LRUCache.java
@@ -1,132 +1,131 @@
 package main;
+//
+//import java.util.HashMap;
+//
+//class Node {
+//  private final int key;
+//  private final int value;
+//  private Node preNode, postNode;
+//
+//  public Node(int key, int value) {
+//    this.key = key;
+//    this.value = value;
+//  }
+//
+//  public int getKey() {
+//    return key;
+//  }
+//
+//  public int getValue() {
+//    return value;
+//  }
+//
+//  public void setPostNode(Node postNode) {
+//    this.postNode = postNode;
+//  }
+//
+//  public void setPreNode(Node preNode) {
+//    this.preNode = preNode;
+//  }
+//
+//  public Node getPostNode() {
+//    return postNode;
+//  }
+//
+//  public Node getPreNode() {
+//    return preNode;
+//  }
+//}
 
-import java.util.HashMap;
-
-class Node{
-    private int key;
-    private int value;
-    private Node preNode,postNode;
-
-    public Node(int key,int value){
-        this.key = key;
-        this.value = value;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setPostNode(Node postNode) {
-        this.postNode = postNode;
-    }
-
-    public void setPreNode(Node preNode) {
-        this.preNode = preNode;
-    }
-
-    public Node getPostNode() {
-        return postNode;
-    }
-
-    public Node getPreNode() {
-        return preNode;
-    }
-}
 
 public class LRUCache {
-    private HashMap<Integer,Node> hashMap ;
-    private int capacity;
-
-    private Node head = null;
-    private Node end = null;
-
-    public LRUCache(int capacity){
-        this.capacity=capacity;
-        hashMap=new HashMap<>();
-    }
-
-    public void add(int key, int value){
-        if (hashMap.size() == capacity){
-            Node node = removeFromHead();
-            hashMap.remove(node.getKey());
-        }
-
-        Node node = new Node(key,value);
-        hashMap.put(key,node);
-        set(node);
-    }
-
-    public void delete(int key){
-        if (hashMap.containsKey(key)){
-            Node node = hashMap.get(key);
-            remove(node);
-        }
-    }
-
-    private Node removeFromHead(){
-        Node r = head;
-        head = head.getPostNode();
-        head.setPreNode(null);
-        return r;
-    }
-
-    public int get(int key){
-        if (hashMap.containsKey(key)){
-            Node node = hashMap.get(key);
-            remove(node);
-            set(node);
-            return node.getValue();
-        }
-        return -1;
-    }
-
-    private void remove(Node node){
-        Node temp= head;
-
-        if(head.getKey() == node.getKey()){
-            head = head.getPostNode();
-            head.setPreNode(null);
-        }
-        else {
-            while (temp != null) {
-                if (node.getKey() == temp.getKey()) {
-                    Node pre = temp.getPreNode();
-                    Node post = temp.getPostNode();
-                    pre.setPostNode(post);
-                    break;
-                }
-            }
-        }
-    }
-
-    private void set(Node node){
-        if (head == null){
-            head= node;
-            end = node;
-        }
-        else{
-            end.setPostNode(node);
-            node.setPreNode(end);
-            end=end.getPostNode();
-        }
-
-    }
-
-    public static void main(String[] args) {
-        LRUCache cache = new LRUCache(2);
-        cache.add(1,10);
-        System.out.println("Add successful");
-        cache.add(5,12);
-        System.out.println(cache.get(5));
-        System.out.println("fetch successful");
-        System.out.println(cache.get(1));
-        System.out.println(cache.get(10));
-        cache.add(6,14);
-        System.out.println(cache.get(5));
-
-    }
+//  private final HashMap<Integer, Node> hashMap;
+//  private final int capacity;
+//
+//  private Node head = null;
+//  private Node end = null;
+//
+//  public LRUCache(int capacity) {
+//    this.capacity = capacity;
+//    hashMap = new HashMap<>();
+//  }
+//
+//  public void add(int key, int value) {
+//    if (hashMap.size() == capacity) {
+//      Node node = removeFromHead();
+//      hashMap.remove(node.getKey());
+//    }
+//
+//    Node node = new Node(key, value);
+//    hashMap.put(key, node);
+//    set(node);
+//  }
+//
+////  public void delete(int key) {
+////    if (hashMap.containsKey(key)) {
+////      Node node = hashMap.get(key);
+////      remove(node);
+////    }
+////  }
+//
+//  private Node removeFromHead() {
+//    Node r = head;
+//    head = head.getPostNode();
+//    head.setPreNode(null);
+//    return r;
+//  }
+//
+//  public int get(int key) {
+//    if (hashMap.containsKey(key)) {
+//      Node node = hashMap.get(key);
+//      remove(node);
+//      set(node);
+//      return node.getValue();
+//    }
+//    return -1;
+//  }
+//
+//  private void remove(Node node) {
+//    Node temp = head;
+//
+//    if (head.getKey() == node.getKey()) {
+//      head = head.getPostNode();
+//      head.setPreNode(null);
+//    } else {
+//      while (temp != null) {
+//        if (node.getKey() == temp.getKey()) {
+//          Node pre = temp.getPreNode();
+//          Node post = temp.getPostNode();
+//          pre.setPostNode(post);
+//          break;
+//        }
+//      }
+//    }
+//  }
+//
+//  private void set(Node node) {
+//    if (head == null) {
+//      head = node;
+//      end = node;
+//    } else {
+//      end.setPostNode(node);
+//      node.setPreNode(end);
+//      end = end.getPostNode();
+//    }
+//
+//  }
+//
+//  public static void main(String[] args) {
+//    LRUCache cache = new LRUCache(2);
+//    cache.add(1, 10);
+//    System.out.println("Add successful");
+//    cache.add(5, 12);
+//    System.out.println(cache.get(5));
+//    System.out.println("fetch successful");
+//    System.out.println(cache.get(1));
+//    System.out.println(cache.get(10));
+//    cache.add(6, 14);
+//    System.out.println(cache.get(5));
+//
+//  }
 }
